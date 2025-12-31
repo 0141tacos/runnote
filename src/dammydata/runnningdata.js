269e9affecc30a8735f66f1ef5d0147e200539fa
Hirotaka Sato <laggyboy@outlook.jp>
2025-12-31 10:34:59 +0900

    グラフ表示

---
+++ src/dammydata/runnningdata.js
@@ -0,0 +1,12 @@
+export default [
+  { date: '2025-07-01T00:00:00Z', pace_sec_per_km: 360 },
+  { date: '2025-07-03T00:00:00Z', pace_sec_per_km: 350 },
+  { date: '2025-07-05T00:00:00Z', pace_sec_per_km: 345 },
+  { date: '2025-07-07T00:00:00Z', pace_sec_per_km: 340 },
+  { date: '2025-07-09T00:00:00Z', pace_sec_per_km: 335 },
+  { date: '2025-07-11T00:00:00Z', pace_sec_per_km: 330 },
+  { date: '2025-07-13T00:00:00Z', pace_sec_per_km: 325 },
+  { date: '2025-07-15T00:00:00Z', pace_sec_per_km: 320 },
+  { date: '2025-07-17T00:00:00Z', pace_sec_per_km: 315 },
+  { date: '2025-07-19T00:00:00Z', pace_sec_per_km: 310 },
+]
