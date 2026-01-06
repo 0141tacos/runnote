8eb16e0f784c169c72660bdb769b9309e13466d4
Hirotaka Sato <laggyboy@outlook.jp>
2026-01-06 21:11:51 +0900

    ダミーデータファイルの名称変更

--- src/views/TopView.vue
+++ src/views/TopView.vue
@@ -10,11 +10,11 @@
 <script setup>
 import { onMounted } from 'vue'
 import { Chart } from 'chart.js/auto'
-import Dammydata from '../dammydata/runnningdata'
+import Dammydata from '../dammydata/runningdata'
 
 onMounted(() => {
   const ctx = document.getElementById('time-chart')
-  const dataLabels = Dammydata.map((rows) => rows.date)
+  const dataLabels = Dammydata.map((rows) => new Date(rows.date).toLocaleDateString('ja-JP', { month: '2-digit', day: '2-digit' }))
   const timeData = Dammydata.map((rows) => rows.pace_sec_per_km)
   console.log(dataLabels)
   new Chart(ctx, {
